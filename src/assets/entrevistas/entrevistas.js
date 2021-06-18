c72de2231a3caf046e912843d3938badeedf1f47
pablococciaglia <50939772+pablococciaglia@users.noreply.github.com>
2021-06-18 18:10:30 +0200

    cambio de plugin para manejar videos
    cambio de plugin para reproducir videos a react-youtube
    cambio en el posicionamiento de "rebecaliscano" en home

--- src/assets/entrevistas/entrevistas.js
+++ src/assets/entrevistas/entrevistas.js
@@ -2,47 +2,47 @@
 
 const entrevistas = [
     {
-        'url': 'https://www.youtube.com/watch?v=Nm0SpaftR24',
+        'url': 'Nm0SpaftR24',
         'programa':'Show Business Extra',
         'entrevistados': 'Mireia Lalaguna, Angela Carrasco, Vanesa Romero, Antonio Banderas' 
     },    
     {
-        'url': 'https://www.youtube.com/watch?v=80Ledg0-Qrw&t=141s',
+        'url': '80Ledg0-Qrw&t=141s',
         'programa':'Juan Apóstol El Más Amado',
         'entrevistados': 'Leonardo García, etc' 
     },
     {
-        'url': 'https://www.youtube.com/watch?v=NXVaBAsDemU',
+        'url': 'NXVaBAsDemU',
         'programa':'Show Business Extra',
         'entrevistados': 'Conduciendo Show Business Extra junto con Poty Castillo' 
     },
     { 
-        'url': 'https://www.youtube.com/watch?v=uFrtwMqp-jI',
+        'url': 'uFrtwMqp-jI',
         'programa':'Latin Angels',
         'entrevistados': 'Paula Suárez' 
     },
     {
-        'url': 'https://www.youtube.com/watch?v=E69exIBjvek',
+        'url': 'E69exIBjvek',
         'programa':'Show Business Extra',
         'entrevistados': 'Nieves Alvarez' 
     },
     {
-        'url': 'https://www.youtube.com/watch?v=8iOBn7N7zNs',
+        'url': '8iOBn7N7zNs',
         'programa':'Show Business Extra',
         'entrevistados': 'Aaron Cobos, Desiree Cordero, Ainoa Cantalapiedra' 
     },
     {
-        'url': 'https://www.youtube.com/watch?v=CaeKLsA5Rqc',
+        'url': 'CaeKLsA5Rqc',
         'programa':'Global Gift Gala Dubai',
         'entrevistados': 'Eva Longoria, María Bravo, Melanie Griffith' 
     },
     {
-        'url': 'https://www.youtube.com/watch?v=uJaYgMbc-mU',
+        'url': 'uJaYgMbc-mU',
         'programa':'Show Business Extra',
         'entrevistados': 'Presentadora de Starlite Marbella Fashion Show' 
     },
     {
-        'url': 'https://www.youtube.com/watch?v=T7Lh-FIR1-g',
+        'url': 'T7Lh-FIR1-g',
         'programa':'Show Business Extra',
         'entrevistados': 'Joaquim de Almeida' 
     },
--- src/components/Interviews.js
+++ src/components/Interviews.js
@@ -1,28 +1,32 @@
 import React from 'react'
-import ReactPlayer from 'react-player/youtube'
 import entrevistas from '../assets/entrevistas/entrevistas'
+import ReactPlayer from 'react-player/lazy'
+import YouTube from 'react-youtube';
 
 export const Interviews = () => {
+
+    const opts = {
+        width: '100%'
+    }
+
     return (
         <>
             <h1 >Entrevistas</h1>
             <div className="row">
-
-
-            
                     {entrevistas.map(entrevista=>(
                         
                         <div key={entrevista.url} className="col s12 m6 l4">
                             <div className="card">
                                 <div className="card-content">
                                     <span className="card-title"><strong>{entrevista.programa}</strong></span>
-                                    <ReactPlayer url={entrevista.url} width="100%" />
+                                    <YouTube videoId={entrevista.url} opts={opts}/>
                                 </div>
                                 <div className="card-action">
                                     <p> {entrevista.entrevistados}</p>
                                 </div>
                             </div>
-                         </div>
+                        </div>
+                        
                     ))} 
             </div>
                 
